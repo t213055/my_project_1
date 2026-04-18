ea6b16c943502e533f692e64993c40491b9101b9
Ryusei <matudaryusei0421@gmail.com>
2026-04-18 16:26:52 +0900

    2026/4/18

--- training_GBRBM/results/result_reader.py
+++ training_GBRBM/results/result_reader.py
@@ -1,7 +1,7 @@
 
 import numpy as np
 
-data = np.load("ll_snh5_beta0.25_20260416_1536.npy")
+data = np.load("ll_snh20_beta4.00_20260418_1529.npy")
 print(data)
 print(data.shape)
 print(data.dtype)
